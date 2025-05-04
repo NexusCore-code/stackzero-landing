857fc669ee82b7c1d909a501667d26286cd9ee28
NexusCore-code <elenasemonin@gmail.com>
2025-05-03 16:17:29 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("stackForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("responseMessage").textContent = "Спасибо! Мы свяжемся с вами.";
+});
